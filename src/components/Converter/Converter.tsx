fbdfbc1eb9dbef5e12a616f710251061f358ded3
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-23 14:45:51 -0300

    feat: converter component created

---
+++ src/components/Converter/Converter.tsx
@@ -0,0 +1,11 @@
+import styles from "../Converter/Converter.module.scss";
+
+const Converter = () => {
+  return (
+    <main className={styles.main}>
+        test
+    </main>
+  )
+};
+
+export default Converter;
