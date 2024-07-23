7b0e30bd3c3d329229d71b47d46ca00e806ab40f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-23 14:45:17 -0300

    feat: footer component created

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../Footer/Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>Image designed by <a href="https://www.freepik.com/">Freepik</a></footer>
+  )
+}
+
+export default Footer
