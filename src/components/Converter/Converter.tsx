2af9913f8888b5d77d1dea36cb8d73dca9c47e91
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-23 15:41:13 -0300

    feat: initial structure created and stylized

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -3,7 +3,26 @@ import styles from "../Converter/Converter.module.scss";
 const Converter = () => {
   return (
     <main className={styles.main}>
-        test
+      <h2>Currency Converter</h2>
+      <label htmlFor="amount-input">Enter amount</label>
+      <input id="amount-input" type="number" />
+      <section className={styles.section}>
+        <div>
+          <label htmlFor="from-select">From</label>
+          <select id="from-select">
+
+          </select>
+        </div>
+        <button>Switch</button>
+        <div>
+          <label htmlFor="to-input">To</label>
+          <select id="to-input">
+
+          </select>
+        </div>
+      </section>
+      <button>Get exchange rate</button>
+      <span>Result</span>
     </main>
   )
 };
