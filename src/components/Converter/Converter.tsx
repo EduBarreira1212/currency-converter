0c7354b0bc0f49576396b79ddc500f5dc885a6cc
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-24 12:28:07 -0300

    feat: using new select component

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -1,5 +1,5 @@
-import currencyCodes from "../../shared/currencyCodes";
 import styles from "../Converter/Converter.module.scss";
+import CurrencySelect from "../CurrencySelect/CurrencySelect";
 
 const Converter = () => {
   return (
@@ -8,23 +8,9 @@ const Converter = () => {
       <label htmlFor="amount-input">Enter amount</label>
       <input id="amount-input" type="number" />
       <section className={styles.section}>
-        <div>
-          <label htmlFor="from-select">From</label>
-          <select id="from-select">
-            {currencyCodes.map((code, index) => (
-              <option key={index} value={code}>{code}</option>
-            ))}
-          </select>
-        </div>
+        <CurrencySelect label="From"/>
         <button>Switch</button>
-        <div>
-          <label htmlFor="to-input">To</label>
-          <select id="to-input">
-            {currencyCodes.map((code, index) => (
-              <option key={index} value={code}>{code}</option>
-            ))}
-          </select>
-        </div>
+        <CurrencySelect label="To"/>
       </section>
       <button>Get exchange rate</button>
       <span>Result</span>
