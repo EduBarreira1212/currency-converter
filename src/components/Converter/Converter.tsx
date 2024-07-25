e0561157e50929de3fc92569996c1de016709088
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-25 16:28:49 -0300

    feat: logic to change currency created

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -2,17 +2,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../Converter/Converter.module.scss";
 import CurrencySelect from "../CurrencySelect/CurrencySelect";
 import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Converter = () => {
+  const [FromCurrency, setFromCurrency] = useState("USD");
+  const [ToCurrency, setToCurrency] = useState("BRL");
+
+  const handleClick = () => {
+    setFromCurrency(ToCurrency);
+    setToCurrency(FromCurrency);
+  }
+
   return (
     <main className={styles.main}>
       <h2>Currency Converter</h2>
       <label htmlFor="amount-input">Enter amount</label>
       <input id="amount-input" type="number" />
       <section className={styles.section}>
-        <CurrencySelect label="From"/>
-        <button><FontAwesomeIcon icon={faRightLeft}/></button>
-        <CurrencySelect label="To"/>
+        <CurrencySelect label="From" selectedCurrency={FromCurrency} handleCurrency={(e) => setFromCurrency(e.target.value)}/>
+        <button onClick={handleClick}><FontAwesomeIcon icon={faRightLeft}/></button>
+        <CurrencySelect label="To" selectedCurrency={ToCurrency} handleCurrency={(e) => setToCurrency(e.target.value)}/>
       </section>
       <button>Get exchange rate</button>
       <span>Result</span>
--- src/components/CurrencySelect/CurrencySelect.tsx
+++ src/components/CurrencySelect/CurrencySelect.tsx
@@ -3,18 +3,21 @@ import styles from "../CurrencySelect/CurrencySelect.module.scss";
 
 interface ICurrencyProps {
     label: string,
+    selectedCurrency: string,
+    handleCurrency: (e: any) => void,
 }
 
 
-const CurrencySelect: React.FC<ICurrencyProps> = ({label}) => {
+const CurrencySelect: React.FC<ICurrencyProps> = ({label, selectedCurrency, handleCurrency}) => {
+  
   return (
     <section className={styles.selectSection}>
       <label htmlFor="select">{label}</label>
       <div>
-        <img src={`https://flagsapi.com/${currencyCodes[0].substring(0, 2)}/flat/64.png`} alt="flag" />
-        <select id="select">
-          {currencyCodes.map((code, index) => (
-            <option key={index} value={code}>{code}</option>
+        <img src={`https://flagsapi.com/${selectedCurrency.substring(0, 2)}/flat/64.png`} alt="flag" />
+        <select id="select" value={selectedCurrency} onChange={handleCurrency}>
+          {currencyCodes.map((code) => (
+            <option key={code} value={code}>{code}</option>
           ))}
         </select>
       </div>
