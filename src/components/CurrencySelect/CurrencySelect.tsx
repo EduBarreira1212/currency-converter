e9a7c3de3ebc02243d6650dd2399cdd9afbfd39f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-25 14:34:25 -0300

    feat: stucturing better the component

--- src/components/CurrencySelect/CurrencySelect.tsx
+++ src/components/CurrencySelect/CurrencySelect.tsx
@@ -1,4 +1,5 @@
 import currencyCodes from "../../shared/currencyCodes";
+import styles from "../CurrencySelect/CurrencySelect.module.scss";
 
 interface ICurrencyProps {
     label: string,
@@ -7,15 +8,17 @@ interface ICurrencyProps {
 
 const CurrencySelect: React.FC<ICurrencyProps> = ({label}) => {
   return (
-    <div>
-          <label htmlFor="select">{label}</label>
-          <img src={`https://flagsapi.com/${currencyCodes[0].substring(0, 2)}/flat/64.png`} alt="flag"/>
-          <select id="select">
-            {currencyCodes.map((code, index) => (
-              <option key={index} value={code}>{code}</option>
-            ))}
-          </select>
-        </div>
+    <section className={styles.selectSection}>
+      <label htmlFor="select">{label}</label>
+      <div>
+        <img src={`https://flagsapi.com/${currencyCodes[0].substring(0, 2)}/flat/64.png`} alt="flag" />
+        <select id="select">
+          {currencyCodes.map((code, index) => (
+            <option key={index} value={code}>{code}</option>
+          ))}
+        </select>
+      </div>
+    </section>
   )
 };
 
