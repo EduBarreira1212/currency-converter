ca6d625aa654131f9d67e430640c2cd8e7a7bc1c
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-27 21:33:40 -0300

    feat: interface added to api response

--- src/services/getCurrency.ts
+++ src/services/getCurrency.ts
@@ -1,12 +1,26 @@
 import axios from "axios"
 
+interface IgetCurrencyResponse {
+    result: string,
+    documentation: string,
+    terms_of_use: string,
+    time_last_update_unix: number,
+    time_last_update_utc: string,
+    time_next_update_unix: number,
+    time_next_update_utc: string,
+    base_code: string,
+    target_code: string,
+    conversion_rate: number,
+    conversion_result: number
+}
+
 const apiKey = process.env.REACT_APP_API_KEY;
 
 export const getCurrency = async (fromCurrency: string, toCurrency: string, amount: number) => {
     try {
-        const response = await axios.get(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurrency}/${toCurrency}/${amount}`);
+        const response = await axios.get<IgetCurrencyResponse>(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurrency}/${toCurrency}/${amount}`);
 
-        return response.data;
+        return response.data.conversion_result;
     } catch (error) {
         console.log("Error fetching data:", error);
     }
