8cad180018d3ce79831122b20a2128181adf9276
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-27 22:09:16 -0300

    feat: creating logic to get and display result

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -2,29 +2,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../Converter/Converter.module.scss";
 import CurrencySelect from "../CurrencySelect/CurrencySelect";
 import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { getCurrency } from "../../services/getCurrency";
 
 const Converter = () => {
   const [FromCurrency, setFromCurrency] = useState("USD");
   const [ToCurrency, setToCurrency] = useState("BRL");
+  const [amount, setAmount] = useState<number>();
+
+  const amountRef = useRef<HTMLInputElement | null>(null);
 
   const handleClick = () => {
     setFromCurrency(ToCurrency);
     setToCurrency(FromCurrency);
   }
 
+  const handleExchangeRateClick = async () => {
+    const currency = await getCurrency(FromCurrency, ToCurrency, Number(amountRef.current?.value));
+    if(currency){
+      setAmount(currency);
+    }
+  }
+
   return (
     <main className={styles.main}>
       <h2>Currency Converter</h2>
       <label htmlFor="amount-input">Enter amount</label>
-      <input id="amount-input" type="number" />
+      <input ref={amountRef} id="amount-input" type="number" />
       <section className={styles.section}>
         <CurrencySelect label="From" selectedCurrency={FromCurrency} handleCurrency={(e) => setFromCurrency(e.target.value)}/>
         <button onClick={handleClick}><FontAwesomeIcon icon={faRightLeft}/></button>
         <CurrencySelect label="To" selectedCurrency={ToCurrency} handleCurrency={(e) => setToCurrency(e.target.value)}/>
       </section>
-      <button>Get exchange rate</button>
-      <span>Result</span>
+      <button onClick={handleExchangeRateClick}>Get exchange rate</button>
+      <span>{`${amount}`}</span>
     </main>
   )
 };
